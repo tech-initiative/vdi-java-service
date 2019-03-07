2956003c7a57e27105eef14c9492d29e08a400fe
1629311 <1629311@KOLGP10CTIC1-05.India.TCS.com>
2019-03-07 16:09:13 +0530

    initial commit

---
+++ src/main/java/com/tcs/tool/SpringBoot2JPAWithHibernateAndH2Application.java
@@ -0,0 +1,15 @@
+package com.tcs.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2JPAWithHibernateAndH2Application {
+
+	//private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2JPAWithHibernateAndH2Application.class, args);
+	}
+ 
+}
